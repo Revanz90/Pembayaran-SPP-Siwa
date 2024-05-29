d1c8b5f6afdca51d332b1bb739ecca7ae2072893
xandroaprilianto <alexandrosandro58@gmail.com>
2024-05-29 10:48:16 +0700

    Perbaiki print laporan PDF dan tanggal pada laporan PDF

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -7,13 +7,5 @@ class DashboardController extends Controller
     public function hitungsurat()
     {
         return view('dashboard');
-        // $anggota = Anggota::count();
-        // $simpanan = Simpanan::count();
-        // $pinjaman = Pinjamans::count();
-        // $angsuran = Angsuran::count();
-
-        // $credit = Credit::where('status_credit', 'diterima')->get()->count();
-
-        // return view('dashboard', ['countmember' => $anggota, 'countsaving' => $simpanan, 'countcredit' => $pinjaman, 'countinstalment' => $angsuran]);
     }
 }
